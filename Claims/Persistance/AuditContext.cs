8042599574f9f8865046b250ff60a7246a7d559f
madhukar <madhukarp95@gmail.com>
2025-06-27 19:53:22 +0530

    Refactored to place DBContext changes under Persistance folder

---
+++ Claims/Persistance/AuditContext.cs
@@ -0,0 +1,14 @@
+﻿using Claims.Models.Auditing;
+using Microsoft.EntityFrameworkCore;
+
+namespace Claims.Persistance
+{
+    public class AuditContext : DbContext
+    {
+        public AuditContext(DbContextOptions<AuditContext> options) : base(options)
+        {
+        }
+        public DbSet<ClaimAudit> ClaimAudits { get; set; }
+        public DbSet<CoverAudit> CoverAudits { get; set; }
+    }
+}
