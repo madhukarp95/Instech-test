8eac561bd6b44e41ef5f8728c2841cc8d13e913c
madhukar <madhukarp95@gmail.com>
2025-06-30 16:04:42 +0530

    restructured DB Services

---
+++ Claims/Extensions/DatabaseServiceExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Claims.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+using Testcontainers.MongoDb;
+using Testcontainers.MsSql;
+
+namespace Claims.Extensions
+{
+    public static class DatabaseServiceExtensions
+    {
+        /// <summary>
+        /// Asynchronously configures the database services for the application.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static async Task ConfigureDatabaseServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Use null-coalescing operator to handle potential null values
+            string mongoDbName = configuration["MongoDb:DatabaseName"] ?? throw new ArgumentException("MongoDB Database name is not configured properly.");
+
+            // Start Test containers for SQL Server and MongoDB
+            var sqlContainer = (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+                    ? new MsSqlBuilder()
+                        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+                    : new()
+                ).Build();
+
+            var mongoContainer = new MongoDbBuilder()
+                .WithImage("mongo:latest")
+                .Build();
+
+            await sqlContainer.StartAsync();
+            await mongoContainer.StartAsync();
+
+            services.AddDbContext<AuditContext>(options =>
+                            options.UseSqlServer(sqlContainer.GetConnectionString()));
+
+            services.AddDbContext<ClaimsContext>(options =>
+                options.UseMongoDB(mongoContainer.GetConnectionString(), mongoDbName));
+        }
+    }
+}
--- Claims/Program.cs
+++ Claims/Program.cs
@@ -2,39 +2,10 @@ using Claims.Extensions;
 using Claims.Persistence;
 using Microsoft.EntityFrameworkCore;
 using MongoDB.Driver;
-using System.Runtime.InteropServices;
-using Testcontainers.MongoDb;
-using Testcontainers.MsSql;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Start Test containers for SQL Server and MongoDB
-var sqlContainer = (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
-        ? new MsSqlBuilder()
-            .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
-        : new()
-
-    ).Build();
-
-var mongoContainer = new MongoDbBuilder()
-    .WithImage("mongo:latest")
-    .Build();
-
-await sqlContainer.StartAsync();
-await mongoContainer.StartAsync();
-
-builder.Services.AddDbContext<AuditContext>(options =>
-                options.UseSqlServer(sqlContainer.GetConnectionString()));
-
-builder.Services.AddDbContext<ClaimsContext>(options =>
-{
-    var client = new MongoClient(mongoContainer.GetConnectionString());
-    var database = client.GetDatabase(builder.Configuration["MongoDb:DatabaseName"]); // Use a default/test database name
-    options.UseMongoDB(database.Client, database.DatabaseNamespace.DatabaseName);
-});
-
 await builder.ConfigureDependenciesAsync();
-
 
 var app = builder.Build();
 
